2b818129fc41fd48e77cbdddd4e17b24c67cdefe
Francisco <francisco@wihphotels.com>
2014-09-24 14:34:35 -0400

    Pre Removal of Loop Issue

--- dump_mysql/dump_dbs.sh
+++ dump_mysql/dump_dbs.sh
@@ -1,33 +1,54 @@
-#!/bin/sh
+#!/bin/bash
+username="root"
+password=""
+hostname="127.0.0.1"
+#databases=( "availability97" "fivede61_dotclear" )
+EXCLUDED_DB="Database|information_schema|performance_schema|mysql|the_test|demo"
+#databases= $(mysql -h 127.0.0.1 -P 3306 --user=root -e "SHOW DATABASES;" | grep -Ev "($EXCLUDED_DB)")
+databases=$( echo "show databases;" | mysql -h 127.0.0.1 -P 3306 --user=root | grep -Ev "($EXCLUDED_DB)" 2>&1)
 
-###
-###
-### Author: Francisco Suarez
-### MySQL DB Table Dumper
-### Description: 
-###
+backup=""
+strip="--strip-components=1"
 
+index=0
 
-MYSQL_USER=""
-MYSQL_PASS=""
-EXCLUDED_DB="Database|information_schema|performance_schema|mysql"
-#TIMESTAMP=$(date +"%F")
-#BACKUP_DIR="/backup/$TIMESTAMP"
-#MYSQL=/usr/bin/mysql
-#MYSQLDUMP=/usr/bin/mysqldump
+#bash -x -o igncr dump.sh
 
-rm -rf dumps/*.sql
-
-databases=`mysql -h 127.0.0.1 -P 3306 --user=root -e "SHOW DATABASES;" | grep -Ev "($EXCLUDED_DB)"`
-
-for db in $databases
+while [ "$index" -lt ${#databases[@]} ]
 do
-	#mysqldump -h 127.0.0.1 -uroot $db > "dumps/$db.sql"
-	use $db
-	dbtables="show tables"
-	for tbl in $dbtables
+	database=${databases[$index]}
+	basename="$database-`date +%Y%m%d`"
+	tmp_path="$basename"
+	tmp_file="$tmp_path.tgz"
+
+	mysql_parameters="--user=$username --password=$password --host=$hostname"
+
+	use_dump=$( echo "SELECT ENGINE FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'the_test' AND TABLE_NAME = 'dc_blog';" | mysql -h 127.0.0.1 -P 3306 --user=root)
+
+	$use_dump
+
+	# mysqlhotcopy -u root -h 127.0.0.1 the_test ./
+	# exit
+	mkdir -p $tmp_path
+
+	for table in `mysql $mysql_parameters --database=$database -e "SHOW TABLES" -B -N`
 	do
-		echo 'inner'
-		#mysqldump -h 127.0.0.1 -uroot $db > "dumps/$db.sql"
+		if [ $table != "omit1" -a $table != "omit2" ];
+		then
+			#mysqldump $mysql_parameters $database $table --single-transaction=true --add-drop-table=true > $tmp_path/$table.sql
+			#mysqlhotcopy -u root -h 127.0.0.1 $database ./
+		else
+			mysqlhotcopy -u root -h 127.0.0.1 $database .
+		fi
 	done
+
+	#tar -czvf $tmp_file $tmp_path $strip
+	#rm -r $tmp_path
+
+	((index++))
 done
+
+# SELECT ENGINE
+# FROM information_schema.TABLES
+# WHERE TABLE_SCHEMA = 'the_test'
+# AND TABLE_NAME = 'dc_blog'
